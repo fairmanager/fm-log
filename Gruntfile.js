4254a4795afc51d1a8710e6040295d837873d500
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-04-24 20:38:20 +0200

    [TASK] Added some grunt work

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig(
+    {
+      pkg: grunt.file.readJSON("package.json"),
+
+      jshint: {
+        options: {
+          jshintrc: true
+        },
+        lib: {
+          src: [
+            "lib/*.js"
+          ]
+        }
+      },
+
+      watch: {
+        files: [ "lib/*.js" ],
+        tasks: [ "jshint" ]
+      }
+    }
+  );
+
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.registerTask("default", [ "jshint" ]);
+};
