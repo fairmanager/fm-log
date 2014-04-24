b45c35e0d171171a10bdc5133afc9f78e8e566cd
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-04-24 21:41:34 +0200

    Some working tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,46 @@
-var assert = require( "assert" );
-var log = require("../lib/log.js");
+var should = require( 'should' );
 
-describe( "Logger", function(){
-  it( "should log", function(){
-    log.info("test");
-  });
-  it( "should log multiline", function(){
-    log.info("test\ntest");
-  });
-});
+describe( "Logger", function() {
+  var log;
+  var consoleLogOrig = console.log;
+  var consoleLog = console.log;
+  var result;
+  beforeEach( function() {
+    log = require( "../lib/log.js" );
+    consoleLog = function() {
+      result = arguments;
+      consoleLogOrig.apply( console, arguments );
+    };
+    console.log = consoleLog;
+  } );
+  afterEach( function() {
+    consoleLog = consoleLogOrig;
+  } );
+
+  it( "should log without errors", function() {
+    log.info( "!" );
+    result[0].should.match( /\[INFO  \] !/ );
+  } );
+
+  it( "should log multiline", function() {
+    log.info( "!\n!" );
+    result.length.should.equal( 1 );
+    result[0].should.match( /\[INFO  \] !/ );
+  } );
+
+  it( "should indent all log levels properly", function() {
+    log.verbose("!");
+    result[0].should.match( /\[DEBUG \] !/ );
+    log.info("!");
+    result[0].should.match( /\[INFO  \] !/ );
+    log.notice("!");
+    result[0].should.match( /\[NOTICE\] !/ );
+    log.warn("!");
+    result[0].should.match( /\[WARN  \] !/ );
+    log.error("!");
+    result[0].should.match( /\[ERROR \] !/ );
+    log.critical("!");
+    result[0].should.match( /\[CRITIC\] !/ );
+  } );
+
+} );
