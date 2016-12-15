47102d024912f26333556c262ae99119b6446273
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-12-15 10:52:59 +0100

    [TEST] Padding fixes

--- test/Logger.js
+++ test/Logger.js
@@ -129,12 +129,6 @@ describe( "Logger", () => {
 		} );
 	} );
 
-	describe( "padding", () => {
-		const MessageFormatter = require( "../lib/MessageFormatter" );
-		MessageFormatter.pad( "1" ).should.equal( "01" );
-		MessageFormatter.pad( "11" ).should.equal( "11" );
-	} );
-
 	describe( "errors", () => {
 		it( "should render them properly", () => {
 			log = require( "../lib/log.js" ).module( "module" ).to( logStream );
